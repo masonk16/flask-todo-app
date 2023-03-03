2d746ec9960de0310e3c14693e0d544a9f163235
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-03 20:57:17 +0200

    Initialize task model.

--- api/models.py
+++ api/models.py
@@ -11,3 +11,14 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'User{self.first_name}{self.email}'
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(255))
+    due_date = db.Column(db.DateTime())
+    status = db.Column(db.String(255))
+    todo_owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'Task{self.task_name}{self.due_date}'
